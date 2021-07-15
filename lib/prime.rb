a58fa95727f32c493057b9faabdda110490adaf3
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-15 11:46:54 -0700

    can tell you the prime numbers in a range of an indefinite value, tested up to 10000 should work higher.
    Co-Authored-By: Kyle Williamson <kylewilliamsonv1@gmail.com>

--- lib/prime.rb
+++ lib/prime.rb
@@ -14,19 +14,19 @@ class Prime
   end
 
   def make_array
-    @numbers_array = (1..@number).to_a
+    @numbers_array = (2..@number).to_a
   end
 
   def divisible_delete
     make_array
     x = 0
     until (x === numbers_array.length-1)
-      x = x + 1
       @numbers_array.each do |number|
         if (number > numbers_array[x]) && (number % numbers_array[x] === 0)
           @numbers_array.delete_at(@numbers_array.index(number))
         end
       end
+      x = x + 1
     end
   end
 end
